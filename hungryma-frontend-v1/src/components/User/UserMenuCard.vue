e6fd7dcb2e2d5b50f9be0255cd66b55ba2e9d909
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-09-03 14:39:02 +0800

    edited usermenucard

---
+++ hungryma-frontend-v1/src/components/User/UserMenuCard.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref } from "vue";
+
+// TO EDIT: temporary variables for testing frontend only
+const foodItem = {
+  name: "Food 1",
+  price: 50.9,
+  categoriesId: 3,
+  imageFood: "",
+};
+
+const imageFood = ref("");
+
+const categoriesArr = [
+  "Western",
+  "Asian",
+  "Fast Foods",
+  "Beverages",
+  "Desserts",
+  "Local Delicacies",
+];
+
+const foodCategory = categoriesArr[foodItem.categoriesId - 1];
+</script>
+<template>
+  <div class="card">
+    <!-- menu image -->
+    <div
+      id="menu-image"
+      class="w-full overflow-hidden rounded-lg shadow-lg h-1/2"
+    >
+      <img
+        :src="
+          imageFood !== ''
+            ? imageFood
+            : 'https://previews.123rf.com/images/danilsneg/danilsneg1706/danilsneg170600158/81077084-pepperoni-pizza-italian-pizza-on-white-background.jpg'
+        "
+        :alt="imageFood"
+        class="object-cover w-full h-full"
+      />
+    </div>
+    <div id="menu-info" class="flex items-center justify-between mt-5">
+      <h3 class="text-lg font-semibold">{{ foodItem.name }}</h3>
+      <p class="text-xs font-medium">RM {{ foodItem.price }}</p>
+    </div>
+    <div
+      id="card-bottom"
+      class="flex items-center justify-between gap-1 px-3 mt-6"
+    >
+      <p class="category-badge">{{ foodCategory || "western " }}</p>
+
+      <button
+        @click="deleteItem"
+        class="flex items-center h-8 gap-2 px-2 py-1 text-xs text-white bg-orange-500 rounded-lg cursor-pointer hover:bg-orange-600"
+      >
+        Add to Cart <i class="material-icons-sharp"> shopping_cart </i>
+      </button>
+    </div>
+  </div>
+</template>
--- hungryma-frontend-v1/src/pages/User/UserAllMenu.vue
+++ hungryma-frontend-v1/src/pages/User/UserAllMenu.vue
@@ -1,13 +1,90 @@
 <script setup>
 import NavbarUser from "../../components/NavbarUser.vue";
+import UserMenuCard from "../../components/User/UserMenuCard.vue";
 import { useCurrentUserStore } from "../../store/currentUser";
 const currentUserStore = useCurrentUserStore();
 const { token, currentUser, setToken, getCurrentUser } = currentUserStore;
+
+// TO EDIT: temporary variable to set number of loops(v-for)
+const displayCount = 5;
 </script>
 <template>
   <NavbarUser userPage="user-all-menu" />
-  <div class="bg-blue-400">
-    <h1>Content for all menu</h1>
-    <p>{{ currentUser }}</p>
-  </div>
+  <!-- menu page -->
+  <section class="flex w-full h-screen">
+    <!-- filter section -->
+    <div class="w-1/5 h-full px-8 pt-10 bg-slate-200">
+      <div class="py-5">
+        <h4 class="mb-3 text-xl font-medium">Categories</h4>
+        <p :class="['underline-offset-2 underline hover:text-orange-600 ']">
+          item 1
+        </p>
+        <p :class="['underline-offset-2 underline hover:text-orange-600 ']">
+          item 1
+        </p>
+        <p :class="['underline-offset-2 underline hover:text-orange-600 ']">
+          item 1
+        </p>
+      </div>
+      <div class="py-5">
+        <h4 class="mb-3 text-xl font-medium">Vendors</h4>
+        <p :class="['underline-offset-2 underline hover:text-orange-600 ']">
+          item 1
+        </p>
+        <p :class="['underline-offset-2 underline hover:text-orange-600 ']">
+          item 1
+        </p>
+        <p :class="['underline-offset-2 underline hover:text-orange-600 ']">
+          item 1
+        </p>
+      </div>
+      <div class="py-5">
+        <h4 class="mb-3 text-xl font-medium">Price</h4>
+        <p :class="['underline-offset-2 underline hover:text-orange-600 ']">
+          item 1
+        </p>
+        <p :class="['underline-offset-2 underline hover:text-orange-600 ']">
+          item 1
+        </p>
+        <p :class="['underline-offset-2 underline hover:text-orange-600 ']">
+          item 1
+        </p>
+      </div>
+    </div>
+
+    <!-- menu list section -->
+    <div class="flex flex-col items-center w-4/5 gap-10 py-8">
+      <!-- menu searchbar section -->
+      <div
+        id="search-bar"
+        class="flex items-center h-12 gap-2 px-5 border-2 border-orange-300 rounded-full w-[450px] focus-within:ring-2 focus-within:ring-yellow-200"
+      >
+        <input
+          type="text"
+          placeholder="Chicken Burger"
+          class="flex-1 px-3 text-gray-500 outline-none"
+        />
+        <i class="text-orange-300 material-icons-sharp"> search </i>
+      </div>
+
+      <!-- menu list container -->
+      <div class="grid grid-cols-3 gap-10 overflow-auto">
+        <UserMenuCard v-for="index in displayCount" :key="index" />
+        <!-- <UserMenuCard /> -->
+      </div>
+    </div>
+  </section>
+
+  <!--footer section -->
+  <section class="flex items-center justify-center h-56 bg-slate-100">
+    <h2 class="text-4xl">Footer</h2>
+  </section>
 </template>
+
+<!-- 
+  TODO: 
+  1. Need to add v-model binding based on the filter links. 
+  2. Display logic is not set yet.
+
+  
+ -->
